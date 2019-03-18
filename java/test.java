a9b83d335d89a06526ed78536380eac8edf17f5e
aiboatum <1329916393@qq.com>
2019-03-18 19:12:18 +0800

    add notes file

---
+++ java/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+/**
+ * 测试获得键盘输入
+ * @author euler
+ */
+public class test{
+    public static void main(String[] args) {
+        System.out.println(add(1,2));
+        System.out.println(add(1,2,3));
+    }
+}
